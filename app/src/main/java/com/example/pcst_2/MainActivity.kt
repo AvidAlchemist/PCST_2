4277100dd1ec7fb2e66dee8de8b9e0f7c9e1d4b8
AvidAlchemist <greatalchemist2016@gmail.com>
2024-11-12 22:41:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pcst_2/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.pcst_2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pcst_2.ui.theme.PCST_2Theme
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val fs = Firebase.firestore
+        fs.collection("games")
+            .document()
+            .set(mapOf("title" to "rsa"))
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PCST_2Theme {
+        Greeting("Android")
+    }
+}
