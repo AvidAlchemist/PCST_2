55f5234489f5f7f31d8f39853006488387b25ecb
AvidAlchemist <greatalchemist2016@gmail.com>
2024-11-14 17:29:56 +0300

    Uploading images

--- app/src/main/java/com/example/pcst_2/MainActivity.kt
+++ app/src/main/java/com/example/pcst_2/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.pcst_2
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -22,18 +30,32 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.pcst_2.data.Game
 import com.example.pcst_2.ui.theme.PCST_2Theme
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.io.ByteArrayOutputStream
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val launcher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.PickVisualMedia()
+            ) { uri ->
+                if (uri == null) return@rememberLauncherForActivityResult
+
+                /*Использование Storage..*/
+
+            }
+
             MainScreen()
         }
     }
@@ -41,12 +63,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    val context = LocalContext.current
     val fs = Firebase.firestore
     val list = remember {
         mutableStateOf(emptyList<Game>())
     }
 
-    fs.collection("games").addSnapshotListener{snapshot, exception ->
+    fs.collection("games").addSnapshotListener { snapshot, exception ->
         list.value = snapshot?.toObjects(Game::class.java) ?: emptyList()
     }
 
@@ -66,12 +89,25 @@ fun MainScreen() {
                         .fillMaxWidth()
                         .padding(10.dp)
                 ) {
-                    Text(
-                        text = game.title, modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentWidth()
-                            .padding(15.dp)
-                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        AsyncImage(
+                            model = R.drawable.joystick,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .height(100.dp)
+                                .width(100.dp)
+                        )
+                        Text(
+                            text = game.title,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentWidth()
+
+                        )
+                    }
                 }
             }
         }
@@ -83,23 +119,7 @@ fun MainScreen() {
                 .fillMaxWidth()
                 .padding(10.dp),
             onClick = {
-                fs.collection("games")
-                    .document()
-                    .set(
-                        Game(
-                            "RSA",
-                            "Theory about RSA",
-                            "Count smth",
-                            "Let's make..",
-                            "Don't forget about",
-                            "1",
-                            "what is",
-                            "2",
-                            "3",
-                            "4",
-                            "5"
-                        )
-                    )
+                saveGame(fs)
             }) {
             Text(
                 text = "Add book"
@@ -107,4 +127,32 @@ fun MainScreen() {
         }
     }
 
+}
+
+private fun bitmapToByteArray(context: Context, uri: Uri): ByteArray {
+    val inputStream = context.contentResolver.openInputStream(uri)
+    val bitmap = BitmapFactory.decodeStream(inputStream)
+    val baos = ByteArrayOutputStream()
+    bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos)
+    return baos.toByteArray()
+}
+
+private fun saveGame(fs: FirebaseFirestore /*,url : String*/) {
+    fs.collection("games")
+        .document()
+        .set(
+            Game(
+                "RSA",
+                "Theory about RSA",
+                "Count smth",
+                "Let's make..",
+                "Don't forget about",
+                "1",
+                "what is",
+                "2",
+                "3",
+                "4",
+                "5"
+            )
+        )
 }
