7e1dd4c2a6eed92b3c4dddf2104cd330dec88663
AvidAlchemist <greatalchemist2016@gmail.com>
2024-11-14 11:22:20 +0300

    Add button for adding and method for reading from storage

--- app/src/main/java/com/example/pcst_2/MainActivity.kt
+++ app/src/main/java/com/example/pcst_2/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.pcst_2.data.Game
 import com.example.pcst_2.ui.theme.PCST_2Theme
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -18,25 +33,78 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val fs = Firebase.firestore
-        fs.collection("games")
-            .document()
-            .set(mapOf("title" to "rsa"))
+        setContent {
+            MainScreen()
+        }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PCST_2Theme {
-        Greeting("Android")
+fun MainScreen() {
+    val fs = Firebase.firestore
+    val list = remember {
+        mutableStateOf(emptyList<Game>())
     }
+
+    fs.collection("games").addSnapshotListener{snapshot, exception ->
+        list.value = snapshot?.toObjects(Game::class.java) ?: emptyList()
+    }
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.8f)
+        ) {
+            items(list.value) { game ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
+                ) {
+                    Text(
+                        text = game.title, modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth()
+                            .padding(15.dp)
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            onClick = {
+                fs.collection("games")
+                    .document()
+                    .set(
+                        Game(
+                            "RSA",
+                            "Theory about RSA",
+                            "Count smth",
+                            "Let's make..",
+                            "Don't forget about",
+                            "1",
+                            "what is",
+                            "2",
+                            "3",
+                            "4",
+                            "5"
+                        )
+                    )
+            }) {
+            Text(
+                text = "Add book"
+            )
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/pcst_2/data/Game.kt
@@ -0,0 +1,16 @@
+package com.example.pcst_2.data
+
+//Нужно задать значение по умолчанию во избежание ошибки
+data class Game(
+    val title : String = "",
+    val theoryText : String = "",
+    val taskTitle : String = "",
+    val taskText : String = "",
+    val taskTip : String = "",
+    val taskCorrect : String = "",
+    val testText : String = "",
+    val testCorrect : String = "",
+    val testSomeAnswer_1 : String = "",
+    val testSomeAnswer_2 : String = "",
+    val testSomeAnswer_3 : String = ""
+)
